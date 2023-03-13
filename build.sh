1f5709a9593ea2bc2d208621e9701c7b73ce279f
luximus-hunter <thomaslipman10@gmail.com>
2023-03-13 13:29:21 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 cd ~/Docker || exit
 git clone https://github.com/luximus-hunter/een-frontend.git frontend
 cd ./frontend || exit
-docker build -t uno-frontend -f Dockerfile .
+docker build -t uno-frontend -f Dockerfile . --no-cache
 cd ~/Docker || exit
 rm -rf ./frontend
 git clone https://github.com/luximus-hunter/een-backend.git backend
 cd ./backend || exit
-docker build -t uno-backend -f Dockerfile .
+docker build -t uno-backend -f Dockerfile . --no-cache
 cd ..
 rm -rf ./backend
 docker rmi $(docker images -f "dangling=true" -q)
